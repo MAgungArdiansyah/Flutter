5f3004e06068a29b90fe9d2f0633f608c4c227e2
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-07-16 17:41:35 +0700

    + Input Widget
    TextField & SwitchButton

--- Belajar Ulang/input/input/lib/main.dart
+++ Belajar Ulang/input/input/lib/main.dart
@@ -9,105 +9,179 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: SwitchButton());
+  }
+}
+
+class _FirstScreen extends StatefulWidget {
+  @override
+  __FirstScreenState createState() => __FirstScreenState();
+}
+
+class __FirstScreenState extends State<_FirstScreen> {
+  String? _name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+              onSubmitted: (String value) {
+                setState(() {
+                  _name = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('Hello, $_name'),
+                      );
+                    });
+              },
+              child: Text('Submit'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _SecondScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  __SecondScreenState createState() => __SecondScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class __SecondScreenState extends State<_SecondScreen> {
+  String _name = '';
+  String? _pswrd;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        appBar: AppBar(
+          title: Text('Text Field'),
+          leading: Icon(Icons.menu),
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
           ],
         ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(16),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Username',
+                        //labelText: 'Username',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        prefixIcon: Icon(Icons.person)),
+                    onSubmitted: (value) {
+                      setState(() {
+                        _name = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(16),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFFEFEBE9)),
+                            borderRadius: BorderRadius.circular(10)),
+                        prefixIcon: Icon(Icons.password)),
+                    obscureText: true,
+                    onSubmitted: (value) {
+                      _pswrd = value;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Text('Hello, $_name'),
+                            );
+                          });
+                    },
+                    child: Text('Login'))
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class SwitchButton extends StatefulWidget {
+  const SwitchButton({Key? key}) : super(key: key);
+
+  @override
+  _SwitchButtonState createState() => _SwitchButtonState();
+}
+
+class _SwitchButtonState extends State<SwitchButton> {
+  bool lightOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Switch Button'),
+        leading: Icon(Icons.menu),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Switch(
+          value: lightOn,
+          onChanged: (bool value) {
+            setState(() {
+              lightOn = value;
+            });
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(lightOn ? 'Light On' : 'Light Off')));
+          }),
     );
   }
 }
