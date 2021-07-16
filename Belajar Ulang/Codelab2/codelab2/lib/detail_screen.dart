7b830a88a2c98bebd1189d681f5176c3c2a6b90a
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-07-16 20:29:22 +0700

    +latihan ui sederhana 2

---
+++ Belajar Ulang/Codelab2/codelab2/lib/detail_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class detailscreen extends StatelessWidget {
+  const detailscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Image.asset('images/farm-house.jpg'),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Text(
+              'Farm House Bandung',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.calendar_today),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text('Open Everyday')
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.schedule),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text('09.00 - 20.00')
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.monetization_on),
+                    SizedBox(height: 8.0),
+                    Text('Rp. 25.000')
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(16),
+            child: Text(
+              'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
