cdc7374e2de4eb095f98cd1cbbb42e45b875bf77
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-07-17 06:57:14 +0700

    + Latihan Simple UI 2

--- Belajar Ulang/Codelab2/codelab2/lib/detail_screen.dart
+++ Belajar Ulang/Codelab2/codelab2/lib/detail_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class detailscreen extends StatelessWidget {
-  const detailscreen({Key? key}) : super(key: key);
+  var informasionTextStyle = TextStyle(fontFamily: 'Oxygen');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: SingleChildScrollView(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -15,9 +16,12 @@ class detailscreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(top: 16.0),
             child: Text(
-              'Farm House Bandung',
+              'FARM HOUSE LEMBANG',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Staatliches'),
             ),
           ),
           Container(
@@ -31,7 +35,10 @@ class detailscreen extends StatelessWidget {
                     SizedBox(
                       height: 8.0,
                     ),
-                    Text('Open Everyday')
+                    Text(
+                      'Open Everyday',
+                      style: informasionTextStyle,
+                    )
                   ],
                 ),
                 Column(
@@ -40,14 +47,20 @@ class detailscreen extends StatelessWidget {
                     SizedBox(
                       height: 8.0,
                     ),
-                    Text('09.00 - 20.00')
+                    Text(
+                      '09.00 - 20.00',
+                      style: informasionTextStyle,
+                    )
                   ],
                 ),
                 Column(
                   children: <Widget>[
                     Icon(Icons.monetization_on),
                     SizedBox(height: 8.0),
-                    Text('Rp. 25.000')
+                    Text(
+                      'Rp. 25.000',
+                      style: informasionTextStyle,
+                    )
                   ],
                 )
               ],
@@ -61,8 +74,38 @@ class detailscreen extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
+          Container(
+            height: 150,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'))),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
-    );
+    ));
   }
 }
